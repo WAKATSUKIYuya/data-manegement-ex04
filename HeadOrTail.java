22a52b5e8463bfae1b3823473ad766b83f36aab2
WAKATSUKIYuya <83757880+WAKATSUKIYuya@users.noreply.github.com>
2022-07-08 11:25:07 +0900

    Create HeadOrTail.java
    HoT: Initial revision

---
+++ HeadOrTail.java
@@ -0,0 +1,25 @@
+import java.util.Random;
+
+public class HeadOrTail {
+    public static void main(String args[]) {
+        System.out.println("Tossing a coin...");
+        
+        Random random = new Random();
+        int headCount = 0;
+        int tailCount = 0;
+
+        for (int i = 0; i < 3; i++) {
+            int num = random.nextInt(2);
+
+            if (num == 0) {
+                System.out.println("Routnd " + (i + 1) + ": Heads");
+                headCount++;
+            }
+            else {
+                System.out.println("Routnd " + (i + 1) + ": Tails");
+                tailCount++;
+            }
+        }
+        System.out.println("Heads: " + headCount + ", Tails: " + tailCount);
+    }
+}
