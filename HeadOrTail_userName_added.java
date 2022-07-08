73bcdf7c46801e7bff1a7a70cc739465c91a290f
WAKATSUKIYuya <83757880+WAKATSUKIYuya@users.noreply.github.com>
2022-07-08 11:33:57 +0900

    Create HeadOrTail_userName_added.java
    User name added

---
+++ HeadOrTail_userName_added.java
@@ -0,0 +1,33 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class HeadOrTail {
+    public static void main(String args[]) {
+
+        System.out.println("Who are you?");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        System.out.println("Hello, " + name + "!");
+        scanner.close();
+
+        System.out.println("Tossing a coin...");
+        
+        Random random = new Random();
+        int headCount = 0;
+        int tailCount = 0;
+
+        for (int i = 0; i < 3; i++) {
+            int num = random.nextInt(2);
+
+            if (num == 0) {
+                System.out.println("Routnd " + (i + 1) + ": Heads");
+                headCount++;
+            }
+            else {
+                System.out.println("Routnd " + (i + 1) + ": Tails");
+                tailCount++;
+            }
+        }
+        System.out.println("Heads: " + headCount + ", Tails: " + tailCount);
+    }
+}
